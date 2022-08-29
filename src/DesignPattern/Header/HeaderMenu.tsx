1b73058fd6b9fc1b9e8c6df4fb7a030590610f6a
EasyPage <74344815+easypage@users.noreply.github.com>
2022-08-29 13:31:46 +0900

    router nav

--- src/DesignPattern/Header/HeaderMenu.tsx
+++ src/DesignPattern/Header/HeaderMenu.tsx
@@ -4,11 +4,8 @@ import MenuList from './MenuList';
 function HeaderMenu() {
   return (
     <div className="flex">
-      <MenuList text="캐릭터 검색" />
-      <MenuList text="랭킹" />
-      <MenuList text="경매장" />
-      <MenuList text="재질" />
-      <MenuList text="커뮤니티" />
+      <MenuList navMenu="캐릭터검색" />
+      <MenuList navMenu="경매장" />
     </div>
   );
 }
