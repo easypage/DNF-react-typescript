8b1a7905ed4c2febe12d844d6010b9e58948d2c5
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-09-01 12:39:40 +0900

    서버리스트interface

--- src/Components/Home/Body/HomeSearchForm.tsx
+++ src/Components/Home/Body/HomeSearchForm.tsx
@@ -5,14 +5,11 @@ function SearchForm() {
   return (
     <div className="flex items-center justify-center w-4/6 h-16 border-4 border-yellow-300 border-solid rounded-full shadow-md">
       <select name="charServer" id="charServer" className=" w-32 h-10 text-center focus:outline-none cursor-pointer">
-        <option value="cain">카인</option>
-        <option value="diregie">디레지에</option>
-        <option value="siroco">시로코</option>
-        <option value="prey">프레이</option>
-        <option value="casillas">카시야스</option>
-        <option value="hilder">힐더</option>
-        <option value="anton">안톤</option>
-        <option value="bakal">바칼</option>
+        {serverList.map(server => (
+          <option value={server.value} key={server.value}>
+            {server.text}
+          </option>
+        ))}
       </select>
 
       <input type="search" className="w-2/3 h-12 outline-0 " />
--- src/asset/js/serverList.ts
+++ src/asset/js/serverList.ts
@@ -1,6 +1,15 @@
 interface serverName {
-  value: string;
-  text: string;
+  value: 'cain' | 'diregie' | 'siroco' | 'prey' | 'casillas' | 'hilder' | 'anton' | 'bakal';
+  text: '카인' | '디레지에' | '시로코' | '프레이' | '카시야스' | '힐더' | '안톤' | '바칼';
 }
 
-export const serverList: serverName[] = [{ value: 'cain', text: '카인' }];
+export const serverList: serverName[] = [
+  { value: 'cain', text: '카인' },
+  { value: 'diregie', text: '디레지에' },
+  { value: 'siroco', text: '시로코' },
+  { value: 'prey', text: '프레이' },
+  { value: 'casillas', text: '카시야스' },
+  { value: 'hilder', text: '힐더' },
+  { value: 'anton', text: '안톤' },
+  { value: 'bakal', text: '바칼' },
+];
