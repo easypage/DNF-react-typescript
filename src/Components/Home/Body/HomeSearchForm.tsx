f2c94d569757ed784b3efbb22650c7006a047a90
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-01 12:18:27 +0900

    asset-js-serverListInterface

--- src/Components/Home/Body/HomeSearchForm.tsx
+++ src/Components/Home/Body/HomeSearchForm.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { serverList } from '../../../asset/js/serverList';
 import './../../../css/searchForm.css';
 function SearchForm() {
   return (
     <div className="flex items-center justify-center w-4/6 h-16 border-4 border-yellow-300 border-solid rounded-full shadow-md">
       <select name="charServer" id="charServer" className=" w-32 h-10 text-center focus:outline-none cursor-pointer">
-        <option value="all">전체</option>
         <option value="cain">카인</option>
         <option value="diregie">디레지에</option>
         <option value="siroco">시로코</option>
@@ -16,6 +16,7 @@ function SearchForm() {
       </select>
 
       <input type="search" className="w-2/3 h-12 outline-0 " />
+
       <button className="w-20 h-12">검색</button>
     </div>
   );
---
+++ src/asset/js/serverList.ts
@@ -0,0 +1,6 @@
+interface serverName {
+  value: string;
+  text: string;
+}
+
+export const serverList: serverName[] = [{ value: 'cain', text: '카인' }];
