6b4d18a714db11b34265586f8337dfe640dd548e
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-09-06 17:44:45 +0900

    데이터호출

--- src/Components/CharSearch/CharSearch.tsx
+++ src/Components/CharSearch/CharSearch.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { UserType } from './CharSearchData';
+interface UserType {
+  data: object;
+}
 
-type UserProps = {
-  // 부모 컴포넌트에 import 해온 타입을 재사용
-  user: UserType;
-};
-function CharSearch({ tr }: UserProps) {
-  return <div>캐릭터페이지입니다.</div>;
+function CharSearch({ data }: UserType) {
+  return <div></div>;
 }
 
 export default CharSearch;
--- src/Components/CharSearch/CharSearchData.tsx
+++ src/Components/CharSearch/CharSearchData.tsx
@@ -2,21 +2,11 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import CharSearch from './CharSearch';
 
-export type UserType = {
-  serverId: string;
-  characterId: string;
-  characterName: string;
-  level: number;
-  jobName: string;
-  jobGrowName: string;
-};
-//7ingout.tistory.com/212 [Hello, 7ingout world!:티스토리]
-
-출처: https: function CharSearchData() {
+function CharSearchData() {
   const params = new URLSearchParams(window.location.search);
   const characterName = params.get('nickname');
   let loop = '';
-  const [characterSearchData, setCharacterSearchData] = useState<UserType[]>([]);
+  const [characterSearchData, setCharacterSearchData] = useState([]);
 
   useEffect(() => {
     if (loop !== '1') {
@@ -26,14 +16,8 @@ export type UserType = {
       });
     }
   }, []);
-  console.log(characterSearchData);
-  return (
-    <div>
-      {characterSearchData.map(tr => (
-        <CharSearch key={tr.characterId} tr={tr} />
-      ))}
-    </div>
-  );
+
+  return <div>{characterSearchData.map(charData => (console.log(charData), (<CharSearch key={charData} data={charData} />)))}</div>;
 }
 
 export default CharSearchData;
