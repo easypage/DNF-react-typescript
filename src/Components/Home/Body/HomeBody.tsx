b811d70236e3310cad1c9c804ebaf8ef3732b161
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-13 13:37:13 +0900

    complete headerEventBanner

--- src/Components/Home/Body/HomeBody.tsx
+++ src/Components/Home/Body/HomeBody.tsx
@@ -5,7 +5,7 @@ import HomeCharacterSearchForm from './HomeCharacterSearchForm';
 function HomeBody() {
   return (
     <div className="flex flex-col  items-center">
-      <img src={BodyLogo} alt="mainlogo" />
+      <img src={BodyLogo} className="mt-6 mb-6" alt="mainlogo" />
       <HomeCharacterSearchForm />
     </div>
   );
