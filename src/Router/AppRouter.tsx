ebd548b680e6b867787e9be019ba21ff35c14a1b
merororo2 <108105625+merororo2@users.noreply.github.com>
2022-09-13 14:10:51 +0900

    fileName수정/ 파일정리

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CharacterCard from '../Components/CharacterCard/CharacterDataCall';
-import CharacterDetailView from '../Components/CharacterDetailView/CharacterDataCall';
+import CharacterDetailView from '../Components/CharacterDetailView/CharacterDetailList';
 import HomeMain from '../Pages/HomeMain';
 
 function AppRouter() {
