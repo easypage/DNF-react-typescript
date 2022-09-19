08da54b757ad176703f7e168de31d7a9d83e2f73
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-19 17:43:15 +0900

    update router

--- src/DesignPattern/Header/MenuList.tsx
+++ src/DesignPattern/Header/MenuList.tsx
@@ -12,12 +12,12 @@ interface RouterMapping {
 }
 
 function MenuList(props: MenuListProps) {
-  const RouterMapping: RouterMapping = { 캐릭터검색: '/', 경매장: '/aution' };
+  const RouterMapping: RouterMapping = { 캐릭터검색: '/characterSearch', 경매장: '/aution' };
 
   return (
     <li className="flex items-center text-xl font-Nanum Gothic font-bold opacity-95 cursor-pointer hoverMenu mr-4 last:mr-0">
       <NavLink to={RouterMapping[props.navMenu]}>
-        <p className="ml-2 shad">{props.navMenu}</p>
+        <p className="ml-2">{props.navMenu}</p>
       </NavLink>
     </li>
   );
