4e8a473bb539a61bd5a32fd808d12ca843c8dc7e
EasyPage <74344815+easypage@users.noreply.github.com>
2022-09-19 13:34:15 +0900

    캐릭터 카드

--- src/Components/CharacterCard/CharacterCardList.tsx
+++ src/Components/CharacterCard/CharacterCardList.tsx
@@ -11,12 +11,16 @@ function CharacterCard({ characterDataValue }: CharacterDataProps) {
       {characterDataValue &&
         characterDataValue.map(data => (
           <Link to={`/charView?serverId=${data.serverId}&characterId=${data.characterId}`} key={data.characterId}>
-            <div className="flex flex-col text-center border-2 border-solid border-gray-400 hover:border-yellow-500 ">
-              <img src={`https://dnf-react-typescript.herokuapp.com/character/characterImage?serverId=${data.serverId}&characterId=${data.characterId}?zoom=1`} alt="img" />
-              <p>{data.characterName}</p>
-              <p>{data.level}</p>
-              <p>{data.jobName}</p>
-              <p>{data.jobGrowName}</p>
+            <div className="relative flex flex-col text-center border-2 border-solid border-gray-400 hover:border-yellow-500 ">
+              <img className="mt-6" src={`https://dnf-react-typescript.herokuapp.com/character/characterImage?serverId=${data.serverId}&characterId=${data.characterId}?zoom=1`} alt="img" />
+              <div className="absolute top-4 text-center w-full">
+                <p>카인</p>
+                <div className="flex justify-center items-center">
+                  <p className="text-lg font-bold">{data.characterName}</p>
+                  <p className="ml-2">{data.level}</p>
+                </div>
+                <p>{data.jobGrowName}</p>
+              </div>
             </div>
           </Link>
         ))}
