6ff9c8eecf6e28382ca7b91c6c92182d60d41b3a
EasyPage <74344815+easypage@users.noreply.github.com>
2022-10-05 11:24:51 +0900

    Update VerticalAdBanner.tsx

--- src/DesignPattern/AdBanner/VerticalAdBanner.tsx
+++ src/DesignPattern/AdBanner/VerticalAdBanner.tsx
@@ -3,7 +3,7 @@ import '../../css/adBanner.css';
 
 function VerticalAdBanner() {
   return (
-    <div className="hidden lg:block">
+    <div className="hidden xl:block">
       <a href="https://github.com/easypage/DNF-react-typescript" target="_blank" className="adBanner w-VerticalBanner h-VerticalBanner absolute right-VerticalAdBannerRight top-10"></a>
     </div>
   );
