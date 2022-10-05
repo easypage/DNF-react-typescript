4e08b632460d124bb306290c5c961e950db6f3d3
EasyPage <74344815+easypage@users.noreply.github.com>
2022-10-05 11:17:39 +0900

    헤더 최소값

--- src/DesignPattern/Header/Header.tsx
+++ src/DesignPattern/Header/Header.tsx
@@ -3,7 +3,7 @@ import HeaderMenu from './HeaderMenu';
 import logo from '../../asset/images/logo.png';
 function Header() {
   return (
-    <div className="relative flex items-center bg-gray-900 h-28  text-yellow-400  z-0 header-background-image">
+    <div className="relative min-w-[1080px] flex items-center bg-gray-900 h-28  text-yellow-400  z-0 header-background-image">
       <a href="/">
         <img src={logo} alt="logoimg" />
       </a>
